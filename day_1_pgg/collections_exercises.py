11b6bdd484a182b0d135aae4535a3186416fa4a9
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-03-02 14:07:14 +0100

    Collections.

--- day_1_pgg/collections_exercises.py
+++ day_1_pgg/collections_exercises.py
@@ -36,7 +36,11 @@ param_types = set()
 for product in products:
     for param_pair in product['description'].split(','):
         param_types.add(param_pair.split(':')[0])
-        if product['product_id'] == 200:
-            raise ValueError()
+
+pprint(param_types)
+
+# To jest dobry przykład, kiedy nie używać comprehenions
+# https://towardsdatascience.com/5-wrong-use-cases-of-python-list-comprehensions-e8455fb75692
+param_types = {param_pair.split(':')[0] for product in products for param_pair in product['description'].split(',')}
 
 pprint(param_types)
