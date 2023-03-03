5e73c01a5e08003994a1dd9c1f748f7dc6c7710a
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-03-03 14:04:45 +0100

    Modules.

---
+++ day_2_pgg/module_usage.py
@@ -0,0 +1,9 @@
+# day_2_pgg/module_usage
+
+import day_2_pgg.modules.lib
+
+print(day_2_pgg.modules.lib.first_name)
+print(day_2_pgg.modules.lib.numbers)
+print(day_2_pgg.modules.lib.square(2))
+
+print(locals())
---
+++ day_2_pgg/modules/lib.py
@@ -0,0 +1,15 @@
+# day_2_pgg/modules/lib.py
+
+def square(number):
+    return number ** 2
+
+
+first_name = 'Piotr'
+numbers = [1, 2, 3]
+
+# __main__
+# day_2_pgg.modules.lib
+if __name__ == '__main__':
+    print(__name__)
+    print('Ala ma kota')
+
