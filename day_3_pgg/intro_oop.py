e13a2e594cd1c12f47a5f0e6673743f1aec433c8
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-03-06 10:16:14 +0100

    OOP

---
+++ day_3_pgg/intro_oop.py
@@ -0,0 +1,41 @@
+# day_3_pgg/intro_oop
+
+class Person:
+    ...
+
+
+osoba1 = Person()
+osoba1.first_name = 'Piotr'
+osoba1.last_name = 'GG'
+
+print(osoba1)
+print(osoba1.first_name)
+print(osoba1.last_name)
+
+osoba2 = Person()
+# print(osoba2.first_name)  # AttributeError: 'Person' object has no attribute 'first_name'
+
+
+class Person:
+    greeting = 'Hi'
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def describe(self, greeting='Hello'):
+        print(f"{greeting}! I am {self.first_name}")
+
+    def __str__(self):
+        return 'Trololo'
+
+
+osoba1 = Person('Jan', 'Kowalski')
+osoba1.describe('Hey')
+
+osoba2 = Person('Ala', 'Nowak')
+osoba2.describe()
+
+# konwersja z obiektu na str
+print(osoba1)
+print(str(osoba1))
