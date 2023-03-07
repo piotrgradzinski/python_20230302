e7643844706df589d4e3af336828928a27125325
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-03-07 09:11:56 +0100

    OOP

--- day_4_pgg/oop_exercises2.py
+++ day_4_pgg/oop_exercises2.py
@@ -1,12 +1,9 @@
 # oop_exercises
 
-"""
-Na podstawie produktu z data/products.py
-stwórz klasę reprezentującą ten produkt.
-"""
 from pprint import pprint
-
+from dataclasses import dataclass
 from data.products import products
+
 
 class MarginError(ValueError):
     pass
@@ -47,7 +44,6 @@ class Product:
 
     @property
     def has_basic_margin(self) -> bool:
-        # return self.standard_cost * (1.0 + Product.MARGIN_BASIC_RATE) <= self.list_price
         return self.standard_cost * (1.0 + self.MARGIN_BASIC_RATE) <= self.list_price
 
     def __eq__(self, other):
@@ -56,7 +52,6 @@ class Product:
         elif isinstance(other, int):
             return self.product_id == other
         else:
-            # return id(self) == id(other)
             return False
 
     @staticmethod
@@ -66,48 +61,6 @@ class Product:
             name, value = param_pair.split(':')
             params[name] = value
         return params
-
-
-# p = Product(product_id=1, product_name="G.Skill Ripjaws V Series", description="Speed:DDR4-3000", standard_cost=450.36, list_price=640.99, category_id=5)
-# print(p)
-
-p = Product.create_from_dict(products[0])
-
-p1 = Product.create_from_dict(products[1])
-p2 = Product.create_from_dict(products[1])
-print(p1, p2)
-print(type(p1), type(p2))
-print(id(p1), id(p2))
-print(p1 == p2)
-print(p1.params)
-print(p.params)
-
-# print(p)
-# print(p.product_id, p.product_name, p.standard_cost)
-
-products_oop = [Product.create_from_dict(p) for p in products]
-# pprint(products_oop)
-
-try:
-    print(p.standard_cost, p.list_price, p.margin, p.has_basic_margin)
-    p.margin = 10
-    print(p.standard_cost, p.list_price, p.margin, p.has_basic_margin)
-# except Exception as e:
-# except ValueError as e:
-except MarginError as e:
-    print('Unable to set margin:', e)
-else:
-    # uruchamiany jeśli nie było wyjątku
-    print('Nie było wyjątku...')
-finally:
-    print('finally uruchomi się zawsze ')
-
-print('Ala ma kota')
-
-print('-' * 30)
-
-
-from dataclasses import dataclass
 
 
 @dataclass
@@ -125,9 +78,6 @@ class Order:
         self._order_items: list[OrderItem] = order_items or []
 
     def __getitem__(self, product_id) -> int:
-        # zwraca OrderItem po kluczu z listy self._order_items
-        # return self._order_items[item]
-        # wyszukać OrderItem taki, którego product_id == item
         for oi in self._order_items:
             if oi.product.product_id == product_id:
                 return oi.quantity
@@ -144,7 +94,6 @@ class Order:
 
     def __delitem__(self, product_id):
         for oi in self._order_items:
-            # if oi.product.product_id == product_id:
             if oi.product == product_id:
                 self._order_items.remove(oi)
                 return
@@ -152,7 +101,6 @@ class Order:
 
     def add_product(self, product: Product, quantity: int):
         for oi in self._order_items:
-            # if oi.product.product_id == product.product_id:
             if oi.product == product:
                 oi.quantity += quantity
                 return
@@ -170,29 +118,15 @@ class Order:
         out.append(f'Total: {self.total_price:.2f}')
         return '\n'.join(out)
 
+
+products_oop = [Product.create_from_dict(p) for p in products]
+
 my_order_items = [
     OrderItem(product=Product.create_from_dict(products[0]), quantity=10),
     OrderItem(product=Product.create_from_dict(products[1]), quantity=20),
 ]
 
 my_order = Order(order_items=my_order_items)
-print(my_order[1])  # my_order[product_id] -> quantity
-try:
-    print(my_order[5])
-except KeyError as e:
-    print(e)
-
-print(my_order[1])
-my_order[1] = 100
-print(my_order[1])
-
-try:
-    my_order[5] = 500
-except KeyError as e:
-    print(e)
-
-del(my_order[1])
-print(my_order._order_items)
 
 my_order.add_product(product=products_oop[0], quantity=10)
 my_order.add_product(product=products_oop[1], quantity=30)
@@ -200,3 +134,12 @@ my_order.add_product(product=products_oop[1], quantity=30)
 print(my_order._order_items)
 print(my_order.total_price)
 print(my_order)
+
+print('-' * 30)
+
+
+
+
+
+
+
