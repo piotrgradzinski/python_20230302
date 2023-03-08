3b7f4c28a7631519d2190318978598ab293b018c
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-03-08 12:02:57 +0100

    Re, ex 1.

---
+++ day_5_pgg/ex_3.py
@@ -0,0 +1,9 @@
+"""
+Wczytać plik emails.txt i odfiltrować poprawne mejle:
+- usunąć białe znaki
+- przerobić na małe litery
+- usunąć duplikaty mejli
+- odflitrować tylko (w miarę) poprawne adresy email
+- wynik zapisać do pliku
+"""
+
