a951e7de1f06bffb364c3b696810a901baa555ee
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-03-08 12:38:13 +0100

    Re, ex 4.

--- day_5_pgg/ex_4.py
+++ day_5_pgg/ex_4.py
@@ -3,3 +3,12 @@ orlen.txt
 - usunięcie spacji między liczbami 3 199 -> 3199
 - zamiana " mln" na "000000"
 """
+import re
+
+with open('orlen.txt') as file:
+    report = file.read()
+
+    report = re.sub(r'', r'', report)
+
+    with open('order_clean.txt', 'w') as output:
+        output.write(report)
