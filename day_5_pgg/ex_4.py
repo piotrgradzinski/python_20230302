284d64f28ea58203ec48f71c91aba1523319e077
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-03-08 12:57:33 +0100

    Re, ex 4.

--- day_5_pgg/ex_4.py
+++ day_5_pgg/ex_4.py
@@ -4,11 +4,20 @@ orlen.txt
 - zamiana " mln" na "000000"
 """
 import re
+from pprint import pprint
 
 with open('orlen.txt') as file:
     report = file.read()
 
-    report = re.sub(r'', r'', report)
+    # usunięcie spacji między liczbami 3 199 -> 3199
+    report = re.sub(r'(\d)\s(\d)', r'\1\2', report)
+    # zamiana " mln" na "000000"
+    report = re.sub(r'(\d)\s+mln', r'\g<1>000000', report)
+
+    # positive look-ahead
+    money = re.findall(r'(\d+)(?=\s?PLN)', report)
+
+    pprint(money)
 
     with open('order_clean.txt', 'w') as output:
         output.write(report)
