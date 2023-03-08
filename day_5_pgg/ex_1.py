0902457d2880bed614fecfdbfa25c99753ac686e
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-03-08 10:48:57 +0100

    Re, ex 1.

--- day_5_pgg/ex_1.py
+++ day_5_pgg/ex_1.py
@@ -2,3 +2,10 @@
 Wczytaj plik daty.txt
 Znajdź wszystkie daty i zamień je na format "D.M.Y r."
 """
+import re
+
+with open('daty.txt', encoding='utf-8') as f:
+    content = f.read()
+    # result = re.sub(r'(\d{4})-(\d{2})-(\d{2})', r'\g<3>-\g<2>-\g<1>', content)
+    result = re.sub(r'(\d{4})-(\d{2})-(\d{2})', r'\3-\2-\1', content)
+    print(result)
