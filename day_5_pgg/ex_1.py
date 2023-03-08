7731dbb064798e41d334b519e6eef05c60e8e359
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-03-08 10:51:54 +0100

    Re, ex 1.

--- day_5_pgg/ex_1.py
+++ day_5_pgg/ex_1.py
@@ -6,6 +6,6 @@ import re
 
 with open('daty.txt', encoding='utf-8') as f:
     content = f.read()
-    # result = re.sub(r'(\d{4})-(\d{2})-(\d{2})', r'\g<3>-\g<2>-\g<1>', content)
-    result = re.sub(r'(\d{4})-(\d{2})-(\d{2})', r'\3-\2-\1', content)
+    # result = re.sub(r'(\d{4})-(\d{2})-(\d{2})', r'\g<3>.\g<2>.\g<1>', content)
+    result = re.sub(r'(\d{4})-(\d{2})-(\d{2})', r'\3.\2.\1', content)
     print(result)
