dbd408aeaed46c9b759033fe4ba69cbdd8371422
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-03-08 11:54:45 +0100

    Re, ex 1.

--- day_5_pgg/ex_2.py
+++ day_5_pgg/ex_2.py
@@ -1,3 +1,13 @@
 """
 Wyciągnąć unikalne domeny z pliku adresy_url.txt
 """
+import re
+
+domains = set()
+with open('adresy_url.txt', encoding='utf-8') as file:
+    for address in file:
+        address = address.strip().lower()
+        match = re.search(r'https?://(?:www\.)?([^/]+)/.*', address)
+        domains.add(match.group(1))
+
+print('\n'.join(sorted(domains)))
